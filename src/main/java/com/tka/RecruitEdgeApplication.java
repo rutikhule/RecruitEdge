870b0fbd898dbe0464ec42e7e3bdf3e157ac2cfb
rutikhule <rutikhule2132@gmail.com>
2025-01-12 12:04:40 +0530

    project files

---
+++ src/main/java/com/tka/RecruitEdgeApplication.java
@@ -0,0 +1,13 @@
+package com.tka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecruitEdgeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecruitEdgeApplication.class, args);
+	}
+
+}
